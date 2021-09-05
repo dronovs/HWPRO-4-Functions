ea3ce016379f83969f85930b01939e91650507eb
dronovs <od270891dss@gmail.com>
2021-09-05 18:42:24 +0300

    update

--- script/script.js
+++ script/script.js
@@ -1,13 +1,27 @@
 let firstOperand = 0;
 let secondOperand = 0;
 let result = 0;
+let resultsArr = [];
+let operator = 0;
 
 function askFirstOperand (operand) {
-    return firstOperand = +prompt('Please enter the first operand');
+    firstOperand = +prompt('Please enter the first operand');
+    if (isNaN(firstOperand)) {
+        while (isNaN(firstOperand)) {
+            firstOperand = +prompt('Please enter a NUMBER!');
+        }
+    }
+    return firstOperand;
 }
 
 function askSecondOperand (operand) {
-    return secondOperand = +prompt('Please enter second operand');
+    secondOperand = +prompt('Please enter second operand');
+    if (isNaN(secondOperand)) {
+        while (isNaN(secondOperand)) {
+            secondOperand = +prompt('Please enter a NUMBER!');
+        }
+    }
+    return secondOperand;
 }
 
 function sums (operand1, operand2) {
@@ -29,18 +43,26 @@ function divides (operand1, operand2) {
 function sinus (operand) {
     return result = Math.sin(operand);
 }
+
 do {
-    let operator = prompt(`Choose one of operations: +, -, *, /, sin`);
+    operator = prompt(`Choose one of operations: +, -, *, /, sin or history`, `+`);
     if (operator === `+`) {
-        console.log(`Operation sum finished with result ${sums(askFirstOperand(), askSecondOperand())}`)
-        console.log(result);
+        alert(`Operation sum finished with result ${sums(askFirstOperand(), askSecondOperand())}`);
+        result = `\nOperation sum finished with result ${result}`;
     } else if (operator === `-`) {
-        console.log(difference(askFirstOperand(), askSecondOperand()));
+        alert(`Operation diff finished with result ${difference(askFirstOperand(), askSecondOperand())}`);
+        result = `\nOperation diff finished with result ${result}`;
     } else if (operator === `*`) {
-        console.log(multiplies(askFirstOperand(), askSecondOperand()));
+        alert(`Operation mult finished with result ${multiplies(askFirstOperand(), askSecondOperand())}`);
+        result = `\nOperation mult finished with result ${result}`;
     } else if (operator === `/`) {
-        console.log(divides(askFirstOperand(), askSecondOperand()));
+        alert(`Operation div finished with result ${divides(askFirstOperand(), askSecondOperand())}`);
+        result = `\nOperation div finished with result ${result}`;
     } else if (operator === `sin`) {
-        console.log(sinus(askFirstOperand()));
+        alert(`Operation sin finished with result ${sinus(askFirstOperand())}`);
+        result = `\nOperation sin finished with result ${result}`;
+    } else if (operator === `history`) {
+        alert(resultsArr);
     }
+    resultsArr.push(result);
 } while (confirm('Any more calculations?'));
